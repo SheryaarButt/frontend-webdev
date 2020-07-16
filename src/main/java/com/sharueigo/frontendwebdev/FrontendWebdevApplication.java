e54eeff9c64c2c1762834efbf4106b3b46e0f73f
Sheryaar <Sheryaar.butt@gmail.com>
2020-07-16 17:26:49 -0400

    -Initial Commit

---
+++ src/main/java/com/sharueigo/frontendwebdev/FrontendWebdevApplication.java
@@ -0,0 +1,13 @@
+package com.sharueigo.frontendwebdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendWebdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendWebdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharueigo/frontendwebdev/FrontendWebdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharueigo.frontendwebdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendWebdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
